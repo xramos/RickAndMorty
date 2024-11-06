35834faa0066e28b94c3a135d36afc9ca9a3e2f2
xramos <xramoscobain@gmail.com>
2024-11-06 13:21:15 +0100

    feat: Adding initial CharacterListView and CharacterListViewModel

---
+++ RickAndMorty/RickAndMorty/Scenes/CharacterList/CharacterListView.swift
@@ -0,0 +1,66 @@
+//
+//  CharacterListView.swift
+//  RickAndMorty
+//
+//  Created by Xavier Ramos Sole on 5/11/24.
+//
+
+import SwiftUI
+
+struct CharacterListView: View {
+    
+    @StateObject var viewModel: CharacterListViewModel = CharacterListViewModel()
+    
+    var body: some View {
+        
+        VStack {
+            
+            content
+            
+        }.task {
+            
+            viewModel.getCharacters()
+        }
+    }
+    
+    @ViewBuilder
+    var content: some View {
+        
+        switch viewModel.state {
+        case .loading:
+            loadingView
+        case .failed:
+            failedView
+        case .loaded:
+            loadedView
+        }
+    }
+    
+    @ViewBuilder
+    var loadingView: some View {
+        
+        ProgressView()
+        
+        Text("Loading Characters")
+            .font(.caption)
+    }
+    
+    @ViewBuilder
+    var failedView: some View {
+        
+        Text("Ops, there has been some error")
+                
+        Button(action: { viewModel.getCharacters() },
+               label: { Text("Try again")})
+    }
+    
+    @ViewBuilder
+    var loadedView: some View {
+        
+        Text("TODO: List Characters")
+    }
+}
+
+#Preview {
+    CharacterListView()
+}
